8f215465f0132c5b9990dcf9dc301a36be178d81
mansouriamn <38742344+mansouriamn@users.noreply.github.com>
2019-01-24 21:37:39 -0800

    changed db

--- models/company.js
+++ models/company.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/tech-connect-test', { useNewUrlParser: true })
+let mongooseurl = 'mongodb://amini.daniel@gmail.com:DUm-8hu-rqQ-JV6@ds249824.mlab.com:49824/heroku_41cjg6ml'
+mongoose.connect(mongooseurl, { useNewUrlParser: true })
 const Schema = mongoose.Schema
 
 const CompanySchema = new Schema({
