48515b3048c97b9f156c5edb8f2258c1874a2806
mansouriamn <38742344+mansouriamn@users.noreply.github.com>
2019-01-24 21:31:28 -0800

    heroku build

---
+++ client/build/precache-manifest.e6eec09dae9f12eb984a125a38cb5f78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e581ebe940c4bcfbaac02526f3ec77f",
+    "url": "/static/media/Loading.3e581ebe.jpg"
+  },
+  {
+    "revision": "ea1af375873a1f945f192f40de978cbd",
+    "url": "/static/media/Desk.ea1af375.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0cffd367efa576082d23",
+    "url": "/static/js/main.0cffd367.chunk.js"
+  },
+  {
+    "revision": "37c5e173f0ad54a3e2ac",
+    "url": "/static/js/1.37c5e173.chunk.js"
+  },
+  {
+    "revision": "0cffd367efa576082d23",
+    "url": "/static/css/main.0f0c91f1.chunk.css"
+  },
+  {
+    "revision": "5cd190d69ccdd52f70bdfb32d8b0c08f",
+    "url": "/index.html"
+  }
+];
